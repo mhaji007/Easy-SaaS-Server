bcf818fe4b67c75accf201bf74edf8354ad933aa
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-04-17 22:45:00 -0400

    Set up initial email and password validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,50 @@
 exports.register = (req, res, next) => {
+  const { email, password } = req.body;
 
-}
+  const validationErrors = [];
+  // Validate the input fields
+  if (!email) {
+    // Define the shape of error object
+    // for the whole application
+    validationErrors.push({
+      code: "VALIDATION_ERROR",
+      field: "email",
+      message: "You must provide an email address",
+    });
+  }
 
-exports.login = (req, res, next) => {
+  const isEmailValid = validateEmail(email);
+  if (email && !isEmailValid) {
+    validationErrors.push({
+      code: "VALIDATION_ERROR",
+      field: "email",
+      message: "Email is not valid",
+    });
+  }
 
-}
+  if (!password) {
+    validationErrors.push({
+      code: "VALIDATION_ERROR",
+      field: "password",
+      message: "You must provide a password",
+    });
+  }
 
-exports.logout = (req, res, next) =>{
-  
+  if (validationErrors.length) {
+    const errorObject = {
+      error: true,
+      errors: validationErrors,
+    };
+    res.send(errorObject);
+    return;
+  }
+};
+
+exports.login = (req, res, next) => {};
+
+exports.logout = (req, res, next) => {};
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {register, login, logout} = require("../controllers/auth")
+const {register, login, logout} = require("../controllers/auth");
+
+router.post("/register", register);
 
 module.exports = router;
