4b9888fb9687cd4fe27ee258593fe7621c560cdc
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-04-17 21:14:14 -0400

    Define user model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+// Schema creates a whitelist of fields
+// that rules can be attached to
+
+// If statement prevents model to be defined
+// again and again upon import
+if (!User) {
+  const userSchema = new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: "Name is required",
+        maxlength: [150, "Name cannot exceed 150 characters"],
+      },
+      email: {
+        type: String,
+        required: "Email is required. Please enter a valid email address",
+        // trim: true,
+        lowercase: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: "Password is required",
+      },
+      role: {
+        type: Number,
+        default: 0,
+      },
+      avatar_url: {
+        type: String,
+        default: "",
+      },
+      images: {
+        type: Array,
+      }
+    },
+    { timestamps: true }
+  );
+  User = mongoose.model("User", userSchema);
+}
+
+module.exports = User;
